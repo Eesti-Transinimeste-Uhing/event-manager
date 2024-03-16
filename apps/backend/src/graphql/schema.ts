5ee9d5ca8889c35dc418a8be1847c2e914f8bee3
DecentM <decentm@decentm.com>
2024-03-16 03:41:05 +0200

    feat: create checkDiscordToken query

--- apps/backend/src/graphql/schema.ts
+++ apps/backend/src/graphql/schema.ts
@@ -2,7 +2,8 @@ import { makeSchema, queryField, queryType } from 'nexus'
 
 import { DiscordCallbackMutation } from './mutations/discord-callback'
 import { User } from './types/user'
+import { CheckDiscordTokenQuery } from './queries/checkDiscordToken'
 
 export const schema = makeSchema({
-  types: [DiscordCallbackMutation, User],
+  types: [DiscordCallbackMutation, CheckDiscordTokenQuery, User],
 })
