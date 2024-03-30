9897f7fb43c64abfde125041b3bcbfe3d5d378ac
DecentM <decentm@decentm.com>
2024-03-30 22:08:20 +0200

    feat: graphql select field

--- apps/web/src/pages/dev-prototyping.vue
+++ apps/web/src/pages/dev-prototyping.vue
@@ -1,9 +1,81 @@
 <script lang="ts" setup>
-import EmptyContent from 'src/components/empty-content.vue'
+import { graphql } from 'src/graphql/generated'
+import { useCursorPagination } from 'src/hooks/use-cursor-pagination'
+import { computed, ref } from 'vue'
+
+const { result, filterText, loading } = useCursorPagination(
+  'templates',
+  graphql(`
+    query SearchTemplates(
+      $filter: [PaginationFilter!]
+      $first: Int
+      $after: String
+      $before: String
+      $last: Int
+      $sort: [PaginationSorting!]
+    ) {
+      templates(
+        filter: $filter
+        first: $first
+        after: $after
+        before: $before
+        last: $last
+        sort: $sort
+      ) {
+        pageInfo {
+          endCursor
+          startCursor
+          hasNextPage
+          hasPreviousPage
+          totalCount
+        }
+        edges {
+          node {
+            id
+            name
+          }
+        }
+      }
+    }
+  `),
+  {
+    defaultFilterColumns: ['name'],
+    defaultSortColumns: ['name'],
+  }
+)
+
+const model = ref(null)
+
+const options = computed(() => {
+  return (
+    result.value?.templates.edges.map((edge) => ({
+      value: edge.node.id,
+      label: edge.node.name,
+    })) ?? []
+  )
+})
 </script>
 
 <style lang="scss" scoped></style>
 
 <template>
-  <empty-content />
+  <q-select
+    borderless
+    v-model="model"
+    use-input
+    hide-selected
+    fill-input
+    label="Lazy filter"
+    :options="options"
+    @input-value="(v) => (filterText = v)"
+    :input-debounce="300"
+    :loading="loading"
+    dropdown-icon="las la-caret-down"
+  >
+    <template v-slot:no-option>
+      <q-item>
+        <q-item-section class="text-grey"> No results </q-item-section>
+      </q-item>
+    </template>
+  </q-select>
 </template>
