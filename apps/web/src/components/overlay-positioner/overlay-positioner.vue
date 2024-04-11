634aa3358512301c5520a8e72bfdfb9bc25a90ff
DecentM <decentm@decentm.com>
2024-04-11 14:35:07 +0300

    feat: create overlay positioner component

--- apps/web/src/components/overlay-positioner/overlay-positioner.vue
+++ apps/web/src/components/overlay-positioner/overlay-positioner.vue
@@ -1,29 +1,47 @@
 <script lang="ts" setup>
 import { TouchPanValue } from 'quasar'
-import { AspectRatio } from 'src/lib/aspect-ratios'
-import { ref, watch } from 'vue'
+import { computed, onMounted, ref, watch } from 'vue'
 
-const props = withDefaults(
-  defineProps<{
-    modelValue: [number, number][]
-    aspectRatio?: number
-  }>(),
-  {
-    aspectRatio: AspectRatio.square,
-  }
-)
+const props = defineProps<{
+  size: [number, number]
+  modelValue: [number, number][]
+}>()
 
 const emit = defineEmits<{
   (event: 'update:model-value', value: [number, number][]): void
 }>()
 
 const targets = ref<HTMLElement[]>([])
-
 const dragging = ref<EventTarget | null>(null)
-
-const offsets = ref<[number, number][]>(props.modelValue.map(() => [0, 0]))
-
+const offsets = ref<[number, number][]>([])
 const parentSize = ref({ width: 0, height: 0 })
+const itemSizes = ref<[number, number][]>([])
+
+const handleItemResize = (index: number, size: { width: number; height: number }) => {
+  const newItemSizes = [...itemSizes.value]
+
+  newItemSizes[index] = [size.width, size.height]
+
+  itemSizes.value = newItemSizes
+}
+
+const normaliseToParent = (input: [number, number]): [number, number] => {
+  return [
+    (input[0] / parentSize.value.width) * props.size[0],
+    (input[1] / parentSize.value.height) * props.size[1],
+  ]
+}
+
+const denormaliseFromParent = (input: [number, number]): [number, number] => {
+  return [
+    (input[0] / props.size[0]) * parentSize.value.width,
+    (input[1] / props.size[1]) * parentSize.value.height,
+  ]
+}
+
+onMounted(() => {
+  offsets.value = props.modelValue.map((offset) => denormaliseFromParent(offset))
+})
 
 const handlePan: TouchPanValue = ({ evt, isFirst, isFinal, delta }) => {
   if (!evt || !delta) return
@@ -34,57 +52,68 @@ const handlePan: TouchPanValue = ({ evt, isFirst, isFinal, delta }) => {
   const newOffsets: [number, number][] = [...offsets.value]
 
   for (const [index, target] of targets.value.entries()) {
-    if (dragging.value === target)
+    if (dragging.value === target) {
       newOffsets[index] = [
-        Math.min(parentSize.value.width, Math.max(0, offsets.value[index][0] + (delta.x || 0))),
-        Math.min(parentSize.value.height, Math.max(0, offsets.value[index][1] + (delta.y || 0))),
+        Math.min(
+          parentSize.value.width - itemSizes.value[index][0],
+          Math.max(0, offsets.value[index][0] + (delta.x || 0))
+        ),
+        Math.min(
+          parentSize.value.height - itemSizes.value[index][1],
+          Math.max(0, offsets.value[index][1] + (delta.y || 0))
+        ),
       ]
+    }
   }
 
   offsets.value = newOffsets
 }
 
 watch(dragging, (newDragging) => {
-  if (!newDragging) emit('update:model-value', offsets.value)
+  if (!newDragging)
+    emit(
+      'update:model-value',
+      offsets.value.map((offset) => normaliseToParent(offset))
+    )
 })
 
-const handleResize = (size: { width: number; height: number }) => {
+const handleParentResize = (size: { width: number; height: number }) => {
   parentSize.value = size
-  console.log(size)
 }
+
+const aspectRatio = computed(() => {
+  return props.size[0] / props.size[1]
+})
 </script>
 
 <style lang="scss" scoped>
 .drag-root {
-  max-height: 100%;
-  max-width: 100%;
-  user-select: none;
   aspect-ratio: v-bind(aspectRatio);
 }
 
-.overlay-positioner {
-  margin: 2px;
-}
-
-.test {
-  background: blue;
-  height: 250px;
-  width: 250px;
+.drag-item {
+  > :deep(*) {
+    pointer-events: none;
+  }
 }
 </style>
 
 <template>
-  <div v-touch-pan.prevent.mouse="handlePan" class="fit bg-red relative-position">
-    <q-resize-observer @resize="handleResize" />
+  <div v-touch-pan.prevent.mouse="handlePan" class="drag-root relative-position">
+    <q-resize-observer @resize="handleParentResize" />
 
-    <div
-      v-for="(item, index) in props.modelValue"
-      :key="index"
-      :style="{ top: `${offsets[index][1]}px`, left: `${offsets[index][0]}px` }"
-      class="test absolute"
-      ref="targets"
-    ></div>
+    <transition-group name="card">
+      <div
+        v-for="(item, index) in offsets"
+        :key="index"
+        :style="{ top: `${item[1]}px`, left: `${item[0]}px` }"
+        class="absolute drag-item"
+        ref="targets"
+      >
+        <q-resize-observer @resize="(s) => handleItemResize(index, s)" />
+
+        <slot :name="`item-${index}`" />
+      </div>
+    </transition-group>
   </div>
-
-  {{ offsets }}
 </template>
--- apps/web/src/pages/dev-prototyping.vue
+++ apps/web/src/pages/dev-prototyping.vue
@@ -3,7 +3,7 @@ import { ref } from 'vue'
 
 import OverlayPositioner from 'src/components/overlay-positioner/overlay-positioner.vue'
 
-const model = ref<[number, number][]>([[0, 0]])
+const model = ref<[number, number][]>([[464.090541632983, 516.7241379310344]])
 </script>
 
 <style lang="scss" scoped>
@@ -14,12 +14,14 @@ const model = ref<[number, number][]>([[0, 0]])
 
 <template>
   <q-card flat bordered class="dev-card">
-    <overlay-positioner v-model="model" :aspect-ratio="1">
+    <overlay-positioner v-model="model" :size="[1920, 1080]">
       <template #item-0>
-        <span>My Item!</span>
+        <q-card>
+          <q-card-section>
+            <span>My Item!</span>
+          </q-card-section>
+        </q-card>
       </template>
     </overlay-positioner>
-
-    {{ model }}
   </q-card>
 </template>
